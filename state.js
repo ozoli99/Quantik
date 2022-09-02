eab766e577561421b88dd8a505f2046438bff25b
Zoltan Orosz <orosz.zoltan99@gmail.com>
2022-09-02 20:52:22 +0200

    Create Player and GameState objects

--- state.js
+++ state.js
@@ -1,5 +1,18 @@
 import { Field } from "./field.js";
 
+const Player = {
+    FIRST_PLAYER: 1,
+    SECOND_PLAYER: 2,
+    NO_PLAYER: 4
+};
+
+const GameState = {
+    START_GAME: 1,
+    NEW_GAME: 2,
+    WON_GAME: 4,
+    SELECT_FIGURE: 8
+};
+
 class AppState {
     width = 4;
     height = 4;
